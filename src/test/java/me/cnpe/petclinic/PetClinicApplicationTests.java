951d17d00d8ac16146dfedb562243523474b2e11
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-02-02 21:53:02 -0300

    first commit

---
+++ src/test/java/me/cnpe/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package me.cnpe.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
